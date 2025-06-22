6566a17e7ee7f046bbd689354650b0e43aa56bb9
مسار التميز <masartamayoz@gmail.com>
2025-06-22 08:55:56 +0100

    Update pricing.js

--- js/pricing.js
+++ js/pricing.js
@@ -1,7 +1,6 @@
-const db = firebase.firestore();
-db.collection('pricing').get().then((snapshot) => {
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-        // تحديث DOM بالأسعار
-    });
+const pricingRef = collection(db, 'pricing');
+getDocs(pricingRef).then((snapshot) => {
+  snapshot.forEach((doc) => {
+    // تحديث DOM بالأسعار
+  });
 });
